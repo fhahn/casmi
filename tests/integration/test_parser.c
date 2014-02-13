cdc7f2df3d9954a4bbfba21923a86fb624963c88
Florian Hahn <flo@fhahn.com>
2014-02-13 19:14:35 +0100

    Add existing test cases and test parsing with ctest

---
+++ tests/integration/test_parser.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "libparse/parser.h"
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "%s: Invalid number of arguments\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+    FILE* f = fopen(argv[1], "rt");
+    if (f == NULL) {
+        fprintf(stderr, "%s: could not open file '%s'\n", argv[0], argv[1]);
+        return EXIT_FAILURE;
+    }
+
+    return parse(f);
+}
