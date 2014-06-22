1a74a21338c48fe2fa75dc6ad975c5d2f79d2ca7
Florian Hahn <flo@fhahn.com>
2014-06-22 22:57:44 +0200

    Change Value to value_t in unit tests

--- tests/unit/libinterpreter/test_symbolic.cpp
+++ tests/unit/libinterpreter/test_symbolic.cpp
@@ -18,9 +18,9 @@ class CheckConditionTest: public ::testing::Test {
 };
 
 TEST_F(CheckConditionTest, check_eq_true) {
-  Value sym(new symbol_t(1));
-  Value a((INT_T)20);
-  Value b((INT_T)20);
+  value_t sym(new symbol_t(1));
+  value_t a((INT_T)20);
+  value_t b((INT_T)20);
 
   symbolic_condition p1(&sym, &a, ExpressionOperation::EQ);
   symbolic_condition p2(&sym, &b, ExpressionOperation::EQ);
@@ -29,9 +29,9 @@ TEST_F(CheckConditionTest, check_eq_true) {
 }
 
 TEST_F(CheckConditionTest, check_eq_false) {
-  Value sym(new symbol_t(1));
-  Value a((INT_T)20);
-  Value b((INT_T)10);
+  value_t sym(new symbol_t(1));
+  value_t a((INT_T)20);
+  value_t b((INT_T)10);
 
   symbolic_condition p1(&sym, &a, ExpressionOperation::EQ);
   symbolic_condition p2(&sym, &b, ExpressionOperation::EQ);
@@ -40,9 +40,9 @@ TEST_F(CheckConditionTest, check_eq_false) {
 }
 
 TEST_F(CheckConditionTest, check_eq_neq_false) {
-  Value sym(new symbol_t(1));
-  Value a((INT_T)20);
-  Value b((INT_T)20);
+  value_t sym(new symbol_t(1));
+  value_t a((INT_T)20);
+  value_t b((INT_T)20);
 
   symbolic_condition p1(&sym, &a, ExpressionOperation::NEQ);
   symbolic_condition p2(&sym, &b, ExpressionOperation::EQ);
@@ -51,9 +51,9 @@ TEST_F(CheckConditionTest, check_eq_neq_false) {
 }
 
 TEST_F(CheckConditionTest, check_neq_true) {
-  Value sym(new symbol_t(1));
-  Value a((INT_T)20);
-  Value b((INT_T)20);
+  value_t sym(new symbol_t(1));
+  value_t a((INT_T)20);
+  value_t b((INT_T)20);
 
   symbolic_condition p1(&sym, &a, ExpressionOperation::NEQ);
   symbolic_condition p2(&sym, &b, ExpressionOperation::NEQ);
@@ -62,9 +62,9 @@ TEST_F(CheckConditionTest, check_neq_true) {
 }
 
 TEST_F(CheckConditionTest, check_neq_true_2) {
-  Value sym(new symbol_t(1));
-  Value a((INT_T)20);
-  Value b((INT_T)30);
+  value_t sym(new symbol_t(1));
+  value_t a((INT_T)20);
+  value_t b((INT_T)30);
 
   symbolic_condition k1(&sym, &a, ExpressionOperation::NEQ);
   symbolic_condition k2(&sym, &b, ExpressionOperation::NEQ);
@@ -74,9 +74,9 @@ TEST_F(CheckConditionTest, check_neq_true_2) {
 }
 
 TEST_F(CheckConditionTest, check_neq_not_found_1) {
-  Value sym(new symbol_t(1));
-  Value a((INT_T)20);
-  Value b((INT_T)30);
+  value_t sym(new symbol_t(1));
+  value_t a((INT_T)20);
+  value_t b((INT_T)30);
 
   symbolic_condition k1(&sym, &a, ExpressionOperation::NEQ);
   symbolic_condition p(&sym, &b, ExpressionOperation::NEQ);
@@ -85,9 +85,9 @@ TEST_F(CheckConditionTest, check_neq_not_found_1) {
 }
 
 TEST_F(CheckConditionTest, check_neq_eq_true) {
-  Value sym(new symbol_t(1));
-  Value a((INT_T)20);
-  Value b((INT_T)30);
+  value_t sym(new symbol_t(1));
+  value_t a((INT_T)20);
+  value_t b((INT_T)30);
 
   symbolic_condition k1(&sym, &a, ExpressionOperation::EQ);
   symbolic_condition p(&sym, &b, ExpressionOperation::NEQ);
@@ -96,8 +96,8 @@ TEST_F(CheckConditionTest, check_neq_eq_true) {
 }
 
 TEST_F(CheckConditionTest, check_neq_eq_false) {
-  Value sym(new symbol_t(1));
-  Value a((INT_T)20);
+  value_t sym(new symbol_t(1));
+  value_t a((INT_T)20);
 
   symbolic_condition k1(&sym, &a, ExpressionOperation::EQ);
   symbolic_condition p(&sym, &a, ExpressionOperation::NEQ);
@@ -106,9 +106,9 @@ TEST_F(CheckConditionTest, check_neq_eq_false) {
 }
 
 TEST_F(CheckConditionTest, check_eq_lessereq_true) {
-  Value sym(new symbol_t(1));
-  Value a((INT_T)20);
-  Value b((INT_T)30);
+  value_t sym(new symbol_t(1));
+  value_t a((INT_T)20);
+  value_t b((INT_T)30);
 
   symbolic_condition k1(&sym, &a, ExpressionOperation::EQ);
   symbolic_condition p(&sym, &b, ExpressionOperation::LESSEREQ);
@@ -117,9 +117,9 @@ TEST_F(CheckConditionTest, check_eq_lessereq_true) {
 }
 
 TEST_F(CheckConditionTest, check_eq_lessereq_false) {
-  Value sym(new symbol_t(1));
-  Value a((INT_T)50);
-  Value b((INT_T)30);
+  value_t sym(new symbol_t(1));
+  value_t a((INT_T)50);
+  value_t b((INT_T)30);
 
   symbolic_condition k1(&sym, &a, ExpressionOperation::EQ);
   symbolic_condition p(&sym, &b, ExpressionOperation::LESSEREQ);
@@ -128,9 +128,9 @@ TEST_F(CheckConditionTest, check_eq_lessereq_false) {
 }
 
 TEST_F(CheckConditionTest, check_lessereq_lessereq_true) {
-  Value sym(new symbol_t(1));
-  Value a((INT_T)50);
-  Value b((INT_T)40);
+  value_t sym(new symbol_t(1));
+  value_t a((INT_T)50);
+  value_t b((INT_T)40);
 
   symbolic_condition k1(&sym, &a, ExpressionOperation::LESSEREQ);
   symbolic_condition p(&sym, &b, ExpressionOperation::LESSEREQ);
@@ -139,9 +139,9 @@ TEST_F(CheckConditionTest, check_lessereq_lessereq_true) {
 }
 
 TEST_F(CheckConditionTest, check_lessereq_lessereq_not_found) {
-  Value sym(new symbol_t(1));
-  Value a((INT_T)50);
-  Value b((INT_T)70);
+  value_t sym(new symbol_t(1));
+  value_t a((INT_T)50);
+  value_t b((INT_T)70);
 
   symbolic_condition k1(&sym, &a, ExpressionOperation::LESSEREQ);
   symbolic_condition p(&sym, &b, ExpressionOperation::LESSEREQ);
@@ -150,9 +150,9 @@ TEST_F(CheckConditionTest, check_lessereq_lessereq_not_found) {
 }
 
 TEST_F(CheckConditionTest, check_greater_lessereq_false) {
-  Value sym(new symbol_t(1));
-  Value a((INT_T)50);
-  Value b((INT_T)40);
+  value_t sym(new symbol_t(1));
+  value_t a((INT_T)50);
+  value_t b((INT_T)40);
 
   symbolic_condition k1(&sym, &a, ExpressionOperation::GREATER);
   symbolic_condition p(&sym, &b, ExpressionOperation::LESSEREQ);
@@ -161,9 +161,9 @@ TEST_F(CheckConditionTest, check_greater_lessereq_false) {
 }
 
 TEST_F(CheckConditionTest, check_greater_lessereq_not_found) {
-  Value sym(new symbol_t(1));
-  Value a((INT_T)50);
-  Value b((INT_T)60);
+  value_t sym(new symbol_t(1));
+  value_t a((INT_T)50);
+  value_t b((INT_T)60);
 
   symbolic_condition k1(&sym, &a, ExpressionOperation::GREATER);
   symbolic_condition p(&sym, &b, ExpressionOperation::LESSEREQ);
@@ -172,9 +172,9 @@ TEST_F(CheckConditionTest, check_greater_lessereq_not_found) {
 }
 
 TEST_F(CheckConditionTest, check_eq_greater_true) {
-  Value sym(new symbol_t(1));
-  Value a((INT_T)50);
-  Value b((INT_T)30);
+  value_t sym(new symbol_t(1));
+  value_t a((INT_T)50);
+  value_t b((INT_T)30);
 
   symbolic_condition k1(&sym, &a, ExpressionOperation::EQ);
   symbolic_condition p(&sym, &b, ExpressionOperation::GREATER);
@@ -183,9 +183,9 @@ TEST_F(CheckConditionTest, check_eq_greater_true) {
 }
 
 TEST_F(CheckConditionTest, check_eq_greater_false) {
-  Value sym(new symbol_t(1));
-  Value a((INT_T)50);
-  Value b((INT_T)60);
+  value_t sym(new symbol_t(1));
+  value_t a((INT_T)50);
+  value_t b((INT_T)60);
 
   symbolic_condition k1(&sym, &a, ExpressionOperation::EQ);
   symbolic_condition p(&sym, &b, ExpressionOperation::GREATER);
@@ -194,9 +194,9 @@ TEST_F(CheckConditionTest, check_eq_greater_false) {
 }
 
 TEST_F(CheckConditionTest, check_lessereq_greater_false) {
-  Value sym(new symbol_t(1));
-  Value a((INT_T)50);
-  Value b((INT_T)60);
+  value_t sym(new symbol_t(1));
+  value_t a((INT_T)50);
+  value_t b((INT_T)60);
 
   symbolic_condition k1(&sym, &a, ExpressionOperation::LESSEREQ);
   symbolic_condition p(&sym, &b, ExpressionOperation::GREATER);
@@ -205,9 +205,9 @@ TEST_F(CheckConditionTest, check_lessereq_greater_false) {
 }
 
 TEST_F(CheckConditionTest, check_greater_greater_true) {
-  Value sym(new symbol_t(1));
-  Value a((INT_T)50);
-  Value b((INT_T)60);
+  value_t sym(new symbol_t(1));
+  value_t a((INT_T)50);
+  value_t b((INT_T)60);
 
   symbolic_condition k1(&sym, &a, ExpressionOperation::GREATER);
   symbolic_condition p(&sym, &b, ExpressionOperation::GREATER);
--- tests/unit/libinterpreter/test_value.cpp
+++ tests/unit/libinterpreter/test_value.cpp
@@ -16,7 +16,7 @@ class ListIteratorTest: public ::testing::Test {
 
 TEST_F(ListIteratorTest, test_iterator_bottom_list) {
   BottomList lst;
-  lst.values = { Value((INT_T)1), Value((INT_T)2), Value((INT_T)3) };
+  lst.values = { value_t((INT_T)1), value_t((INT_T)2), value_t((INT_T)3) };
 
   auto iter = lst.begin();
   EXPECT_EQ(3, (*iter).value.integer);
@@ -33,19 +33,19 @@ TEST_F(ListIteratorTest, test_iterator_bottom_list) {
 
 TEST_F(ListIteratorTest, test_iterator_eq_begin_end) {
   BottomList list;
-  list.values = { Value((INT_T)1), Value((INT_T)2), Value((INT_T)3) };
+  list.values = { value_t((INT_T)1), value_t((INT_T)2), value_t((INT_T)3) };
 
   EXPECT_TRUE(list.begin() != list.end());
 }
 
 TEST_F(ListIteratorTest, test_iterator_head_and_tail_list) {
   BottomList list;
-  list.values = { Value((INT_T)1), Value((INT_T)2), Value((INT_T)3) };
+  list.values = { value_t((INT_T)1), value_t((INT_T)2), value_t((INT_T)3) };
 
-  TailList tail(nullptr, Value((INT_T)5));
+  TailList tail(nullptr, value_t((INT_T)5));
   list.tail = &tail;
 
-  HeadList head(&list, Value((INT_T)4));
+  HeadList head(&list, value_t((INT_T)4));
 
   auto iter = head.begin();
   EXPECT_EQ(4, (*iter).value.integer);
@@ -74,12 +74,12 @@ class ListTest: public ::testing::Test {
 
 TEST_F(ListTest, test_list_eq_sublists) {
   BottomList list;
-  list.values = { Value((INT_T)1), Value((INT_T)2), Value((INT_T)3) };
+  list.values = { value_t((INT_T)1), value_t((INT_T)2), value_t((INT_T)3) };
 
-  TailList tail(nullptr, Value((INT_T)5));
+  TailList tail(nullptr, value_t((INT_T)5));
   list.tail = &tail;
 
-  HeadList head(&list, Value((INT_T)4));
+  HeadList head(&list, value_t((INT_T)4));
 
   EXPECT_EQ(true, head == head);
   EXPECT_EQ(true, list == list);
@@ -91,13 +91,13 @@ TEST_F(ListTest, test_list_eq_sublists) {
 }
 
 TEST_F(ListTest, test_list_eq_head_lists) {
-  HeadList h11(nullptr, Value((INT_T)3));
-  HeadList h12(&h11, Value((INT_T)2));
-  HeadList h13(&h12, Value((INT_T)1));
+  HeadList h11(nullptr, value_t((INT_T)3));
+  HeadList h12(&h11, value_t((INT_T)2));
+  HeadList h13(&h12, value_t((INT_T)1));
 
-  HeadList h21(nullptr, Value((INT_T)3));
-  HeadList h22(&h21, Value((INT_T)2));
-  HeadList h23(&h22, Value((INT_T)1));
+  HeadList h21(nullptr, value_t((INT_T)3));
+  HeadList h22(&h21, value_t((INT_T)2));
+  HeadList h23(&h22, value_t((INT_T)1));
 
   EXPECT_TRUE(h11 == h21);
   EXPECT_TRUE(h11 != h22);
@@ -107,18 +107,18 @@ TEST_F(ListTest, test_list_eq_head_lists) {
 }
 
 TEST_F(ListTest, test_list_eq_head_bottom_lists) {
-  HeadList h1(nullptr, Value((INT_T)3));
-  HeadList h2(&h1, Value((INT_T)2));
-  HeadList h3(&h2, Value((INT_T)1));
+  HeadList h1(nullptr, value_t((INT_T)3));
+  HeadList h2(&h1, value_t((INT_T)2));
+  HeadList h3(&h2, value_t((INT_T)1));
 
   BottomList bottom3;
-  bottom3.values = { Value((INT_T)3), Value((INT_T)2), Value((INT_T)1) };
+  bottom3.values = { value_t((INT_T)3), value_t((INT_T)2), value_t((INT_T)1) };
 
   BottomList bottom2;
-  bottom2.values = { Value((INT_T)3), Value((INT_T)2) };
+  bottom2.values = { value_t((INT_T)3), value_t((INT_T)2) };
 
   BottomList bottom1;
-  bottom1.values = { Value((INT_T)3) };
+  bottom1.values = { value_t((INT_T)3) };
 
   EXPECT_TRUE(bottom3 == h3);
   EXPECT_TRUE(bottom2 == h2);
